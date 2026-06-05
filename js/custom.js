ccb99de2b117e99d084eeaa98dcdb122facbca40
Liam <lrose1@users.noreply.github.com>
2026-06-05 02:16:40 -0700

    Update custom.js for speed

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,7 @@
-
- /* jQuery Preloader
+/* jQuery Preloader
   -----------------------------------------------*/
 $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets
 });
 
 
@@ -26,75 +25,21 @@ $(document).ready(function() {
   });
 
 
-  /* BxSlider
-    -----------------------------------------------*/
-  (function (window, $) {
-  'use strict';
-
-  // Cache document for fast access.
-  var document = window.document;
-
-  function mainSlider() {
-    $('.bxslider').bxSlider({
-		auto: true,
-      pagerCustom: '#bx-pager',
-      mode: 'fade',
-      nextText: '',
-      prevText: ''
-    });
-  }
-
-  mainSlider();
-
-  var $links = $(".bx-wrapper .bx-controls-direction a, #bx-pager a");
-  $links.click(function(){
-     $(".slider-caption").removeClass('animated fadeInLeft');
-     $(".slider-caption").addClass('animated fadeInLeft');
-  });
-
-  $(".bx-controls").addClass('container');
-  $(".bx-next").addClass('fa fa-angle-right');
-  $(".bx-prev").addClass('fa fa-angle-left');
-
-
-  })(window, jQuery);
-
-
-   /* Owl Carousel
-    -----------------------------------------------*/
-  $(document).ready(function() {
-    $("#owl-work").owlCarousel({
-      autoPlay: 3000,
-      items : 3,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3],
-    });
-  });
-  
-
-  /* Parallax section
+  /* Parallax section  (desktop only)
+     Skipped on phones/tablets: scroll-driven parallax + fixed
+     backgrounds are janky or unsupported on mobile browsers.
     -----------------------------------------------*/
   function initParallax() {
     $('#work').parallax("100%", 0.3);
     $('#about').parallax("100%", 0.2);
-    $('#team').parallax("100%", 0.3);
-    $('#portfolio').parallax("100%", 0.1);
-    $('#plan').parallax("100%", 0.3);
-    $('#contact').parallax("100%", 0.2);
   }
-  initParallax();
-
-
-  /* Nivo lightbox
-    -----------------------------------------------*/
-  $('#portfolio .col-md-4 a').nivoLightbox({
-        effect: 'fadeScale',
-    });
+  if (window.matchMedia('(min-width: 1025px)').matches) {
+    initParallax();
+  }
 
 
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
 
-  });
-
+});
